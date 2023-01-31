c50a27096908f98b8249e156f0ac0400afcdc09c
fiorellaratti <110804082+fiorellaratti@users.noreply.github.com>
2023-01-31 11:58:09 -0500

    Initial Commit

---
+++ MelodyMind/MelodyMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyMindApp.swift
+//  MelodyMind
+//
+//  Created by Fiorella Ratti on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MelodyMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MelodyMindUITests/MelodyMindUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MelodyMindUITestsLaunchTests.swift
+//  MelodyMindUITests
+//
+//  Created by Fiorella Ratti on 1/31/23.
+//
+
+import XCTest
+
+class MelodyMindUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
